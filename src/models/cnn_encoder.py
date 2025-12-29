c3dcd7f8a2828fe0b1f0aa17a549fb0a4d2889c8
Yehonatan <yehonatan768@gmail.com>
2025-12-29 17:26:35 +0200

    added batch normalization

--- src/models/cnn_encoder.py
+++ src/models/cnn_encoder.py
@@ -40,24 +40,28 @@ class CNNEncoder(nn.Module):
         # valid conv => padding=0, stride=1
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels, 64, kernel_size=10, stride=1, padding=0),
+            nn.BatchNorm2d(64),
             nn.LeakyReLU(negative_slope=0.01, inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
 
         self.conv2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=7, stride=1, padding=0),
+            nn.BatchNorm2d(128),
             nn.LeakyReLU(negative_slope=0.01, inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
 
         self.conv3 = nn.Sequential(
             nn.Conv2d(128, 128, kernel_size=4, stride=1, padding=0),
+            nn.BatchNorm2d(128),
             nn.LeakyReLU(negative_slope=0.01, inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
 
         self.conv4 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=4, stride=1, padding=0),
+            nn.BatchNorm2d(256),
             nn.LeakyReLU(negative_slope=0.01, inplace=True),
         )
 
